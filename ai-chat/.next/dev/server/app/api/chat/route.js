8d872f8164064272ff8c01be328342ca764d9813
Ranim Taieb <ranim.taieb@univ-constantine2.dz>
2026-04-25 12:14:31 +0100

    feat: initialize Next.js project structure and documentation

---
+++ ai-chat/.next/dev/server/app/api/chat/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/chat/route.js")
+R.c("server/chunks/app_api_chat_route_ts_0929wkt._.js")
+R.c("server/chunks/node_modules_next_0hholg5._.js")
+R.c("server/chunks/[root-of-the-server]__0di5i~j._.js")
+R.c("server/chunks/_next-internal_server_app_api_chat_route_actions_0tmcf6..js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/chat/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/chat/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
